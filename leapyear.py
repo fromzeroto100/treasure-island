ce66cbf6c30a6026b62061dc5f045251cf6132ec
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-10 19:44:02 +0100

    new4

---
+++ leapyear.py
@@ -0,0 +1,14 @@
+year = int(input("Enter a random year: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("This is a leap year")
+        else:
+            print("This is not a leap year.")
+
+    else:
+        print("This is a leap year.")    
+
+else:
+    print("This is not a leap year.")
