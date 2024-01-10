cba2520d23de77326e913c70b51c4ded1a2dddd4
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-10 19:07:38 +0100

    new2

--- task1.py
+++ task1.py
@@ -4,6 +4,13 @@ height = int(input("What is your height in cm? "))
 
 if height > 120:
     print("You can ride.")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("You should pay 7$")
 
+    elif age <= 18:
+          print("You should pay 10$")   
+    else:
+        print("You should pay 12$. ")
 else:
-    print("Sorry, you cannot ride.")    
+    print("Sorry, you cannot ride.")   
